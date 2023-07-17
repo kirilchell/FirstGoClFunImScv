71e98cccd94d40c6d219b93ce1d98e0cb7e6cd48
kirilchell <138332858+kirilchell@users.noreply.github.com>
2023-07-17 16:00:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,23 +19,16 @@ from googleapiclient.errors import HttpError
 import gc as garbage_collector
 import chardet
 
-
-num_files = 6
-
-
+num_files = 10
 # Set up logging
 logging.basicConfig(level=logging.INFO)
-
-
 drive_disk = 'https://drive.google.com/drive/folders/1vTrm1w6YsGbMv4AVLr-GdYdGdbGHooCw'
 parent_folder_id = '1vTrm1w6YsGbMv4AVLr-GdYdGdbGHooCw'  # id папки в Google Drive
-
-
 # Установка переменных окружения
 os.environ['ONLINER_EMAIL'] = 'Watchshop'
 os.environ['ONLINER_PASSWORD'] = 'O2203833'
 filename = 'b2bonlinerAerae'
-chunksize = 200000
+chunksize = 100000
 
 def main(event, context):
     email = os.getenv('ONLINER_EMAIL')
@@ -50,18 +43,12 @@ def main(event, context):
         authenticate(session, password, email)
         download_file(session, url_onliner_file, data_file_path)
 
-        credentials = get_credentials()
-
-        parent_id = "1vTrm1w6YsGbMv4AVLr-GdYdGdbGHooCw"
-        # upload_to_drive(data_file_path, parent_id, credentials, filename)
+        key_filenames = ['inner-nuance-389811-05efdb1df532.json', 'inner-nuance-389811-13fe8ddc7b28.json']
+        credentials_list = [get_credentials(key_filename) for key_filename in key_filenames]
         
-        chunks = process_files(data_file_path, chunksize) # перемещено перед удалением файла
-
-        file_objects, service = create_and_move_files(filename, credentials, parent_folder_id, num_files)
-
-        spreadsheet = process_last_modified_file(file_objects, service)
-
-        upload_to_gsheetsgapi(credentials, spreadsheet, chunks)
+        file_objects, service_drive = create_and_move_files(filename, credentials_list[0], parent_folder_id, num_files)
+        
+        process_and_upload_files(data_file_path, chunksize, file_objects, service_drive, credentials_list) # используется новая функция обработки и загрузки
 
         if os.path.isfile(data_file_path):
             os.remove(data_file_path)
@@ -78,29 +65,13 @@ def main(event, context):
 
     return 'Файл успешно загружен.'
 
-
-def resilient_request_execute(request, max_retries=5, sleep_time=5):
-    for attempt in range(max_retries):
-        try:
-            return request.execute()
-        except HttpError as e:
-            if e.resp.status != 500:
-                # If the error is not an internal server error, raise it.
-                raise e
-            else:
-                print(f"Internal Server Error, retrying... (Attempt {attempt + 1})")
-                time.sleep(sleep_time)
-    # If all the retries failed, raise an exception.
-    raise Exception("All retries failed.")
-
-
-def get_credentials():
+def get_credentials(key_filename):
     # Создайте клиент Cloud Storage.
     storage_client = storage.Client()
 
     # Получите объект Blob для файла ключа сервисного аккаунта.
     bucket = storage_client.get_bucket('ia_sam')
-    blob = bucket.blob('inner-nuance-389811-05efdb1df532.json')
+    blob = bucket.blob(key_filename)
 
     # Скачайте JSON файл ключа сервисного аккаунта.
     key_json_string = blob.download_as_text()
@@ -141,12 +112,20 @@ def download_file(session, url, local_filename):
     else:
         print(f'Ошибка при скачивании файла. Код статуса: {r.status_code}')
 
-
-
-
 def create_and_move_files(filename, credentials, parent_folder_id, num_files):
-    gc = gspread.authorize(credentials)
-    files = gc.list_spreadsheet_files()
+    try:
+        gc = gspread.authorize(credentials)
+        print("Authorized successfully.")
+    except Exception as e:
+        print("Error authorizing: ", e)
+        return None
+
+    try:
+        files = gc.list_spreadsheet_files()
+        print("Files retrieved successfully.")
+    except Exception as e:
+        print("Error retrieving files: ", e)
+        return None
 
     # Создание списка файлов и их имен
     file_names = [f"{filename}_{i}" for i in range(num_files)]
@@ -154,34 +133,52 @@ def create_and_move_files(filename, credentials, parent_folder_id, num_files):
 
     # Создание или открытие файлов
     for name in file_names:
-        file_exists = any(file['name'] == name for file in files)
-        if not file_exists:
-            file_objects.append(gc.create(name))
-        else:
-            file_objects.append(gc.open(name))
+        try:
+            file_exists = any(file['name'] == name for file in files)
+            if not file_exists:
+                file_objects.append(gc.create(name))
+            else:
+                file_objects.append(gc.open(name))
+            print(f"File {name} handled successfully.")
+        except Exception as e:
+            print(f"Error handling file {name}: ", e)
+            return None
 
     # Перемещение файлов в требуемую папку
-    service = build('drive', 'v3', credentials=credentials)
+    try:
+        service_drive = build('drive', 'v3', credentials=credentials)
+        print("Drive service built successfully.")
+    except Exception as e:
+        print("Error building drive service: ", e)
+        return None
+
     for file in file_objects:
-        file_id = file.id
-        file_info = service.files().get(fileId=file_id, fields='parents').execute()
-        current_parents = set(file_info.get('parents', []))
-        if parent_folder_id not in current_parents:
-            previous_parents = ",".join(current_parents)
-            service.files().update(
-                fileId=file_id,
-                addParents=parent_folder_id,
-                removeParents=previous_parents,
-                fields='id, parents').execute()
+        try:
+            file_id = file.id
+            file_info = service_drive.files().get(fileId=file_id, fields='parents').execute()
+            current_parents = set(file_info.get('parents', []))
+            if parent_folder_id not in current_parents:
+                previous_parents = ",".join(current_parents)
+                service_drive.files().update(
+                    fileId=file_id,
+                    addParents=parent_folder_id,
+                    removeParents=previous_parents,
+                    fields='id, parents').execute()
+            print(f"File {file_id} moved successfully.")
+        except Exception as e:
+            print(f"Error moving file {file_id}: ", e)
+            return None
 
-    return file_objects, service
+    return file_objects, service_drive
 
-def process_last_modified_file(file_objects, service):
+def process_last_modified_file(file_objects, service_drive):
+
+  
     # Получение последнего измененного файла
     last_modified_file = min(
         file_objects,
         key=lambda file: datetime.datetime.fromisoformat(
-            service.files().get(fileId=file.id, fields='modifiedTime')
+            service_drive.files().get(fileId=file.id, fields='modifiedTime')
             .execute()['modifiedTime'].rstrip('Z')
         )
     )
@@ -214,60 +211,112 @@ def detect_encoding(file_path, num_bytes=10000):
     result = chardet.detect(rawdata)
     return result['encoding']
 
+def process_and_upload_files(local_file_path, chunksize, file_objects, service_drive, credentials_list): 
 
-def reauthorize(credentials):
-        print("Reauthorizing credentials...")
-        gc = gspread.authorize(credentials)
-        print("Credentials reauthorized.")
-        return gc.open_by_key(spreadsheet_id)  # Возвращаем новый объект Spreadsheet
-
-def process_files(local_file_path, chunksize): 
-  
     try: 
         logging.info("Unzipping file...") 
         os.system('gunzip -c ' + local_file_path + ' > ' + local_file_path[:-3]) 
         logging.info("File unzipped.") 
-  
+
         csv_file = local_file_path[:-3] 
-  
-         
+
         header = None 
         chunks = [] 
 
         logging.info("Reading and processing CSV file...") 
         encoding = detect_encoding(csv_file) 
         logging.info(f"Detected encoding: {encoding}")  # вывод кодировки в логи 
+
+        spreadsheet_ids = set()  # для хранения уникальных id таблиц
+
+        logging.info("Beginning chunk processing...")
         for chunk_id, chunk in enumerate(pd.read_csv(csv_file, encoding=encoding, sep=';', chunksize=chunksize, dtype=str)): 
             logging.info(f'Processing chunk number: {chunk_id}') 
-  
+
             if header is None: 
                 logging.info("Processing header...") 
                 header = chunk.columns.values[:8].tolist() + ['Инфо Магазин'] 
-  
+                logging.info("Header processed.")
+
             logging.info("Processing chunk data...") 
             chunk['Инфо Магазин'] = chunk.iloc[:, 8:].apply(lambda row: '_'.join(row.dropna().astype(str)), axis=1) 
-  
-            logging.info("Before selecting columns...") 
+            logging.info("Chunk data processed.")
             chunk = chunk[header] 
-            logging.info("After selecting columns...") 
-  
-            logging.info("Converting data to string format...") 
             chunk = chunk.astype(str) 
-            logging.info("Data converted.") 
-  
-            chunks.append(chunk) 
 
+            spreadsheet = process_last_modified_file(file_objects, service_drive)
+            spreadsheet_id = upload_to_gsheetsgapi(credentials_list, file_objects, service_drive, [chunk], spreadsheet)
+            spreadsheet_ids.add(spreadsheet_id)
+            logging.info("Chunk uploaded.")
+
+        logging.info("Beginning renaming process...")
+
+        credentials = credentials_list[0]
+        service_sheet = build('sheets', 'v4', credentials=credentials) 
+        gc = gspread.authorize(credentials)
+        
+        for spreadsheet_id in spreadsheet_ids: 
+            try:
+                logging.info(f"Renaming sheet to 'ready' in spreadsheet {spreadsheet_id}...") 
+                spreadsheet = gc.open_by_key(spreadsheet_id)
+                worksheet = spreadsheet.worksheet("transit") 
+                worksheet_id = worksheet.id
+                request = service_sheet.spreadsheets().batchUpdate( 
+                    spreadsheetId=spreadsheet_id, 
+                    body={ 
+                        "requests": [ 
+                            { 
+                                "updateSheetProperties": { 
+                                    "properties": { 
+                                        "sheetId": worksheet_id,
+                                        "title": "ready" 
+                                    }, 
+                                    "fields": "title" 
+                                } 
+                            } 
+                        ] 
+                    } 
+                ) 
+                logging.info("Request prepared.")
+                response = request.execute() 
+                logging.info("Request executed.")
+            except Exception as e: 
+                logging.error(f"Error renaming sheet: {str(e)}")  # добавляем str(e) для вывода подробностей ошибки
     except Exception as e: 
         logging.error(f"An error occurred: {e}") 
 
     finally: 
-        logging.info("Done processing files.") 
-        return chunks 
-
-from googleapiclient.discovery import build
+        logging.info("Done processing and uploading files.")
 
 
-def append_datagapi(df, service, spreadsheet_id, worksheet_id, chunk_size=50000): 
+def upload_to_gsheetsgapi(credentials_list, file_objects, service_drive, chunks, spreadsheet): 
+
+    credentials = credentials_list[0]
+    try: 
+        print("Authorizing credentials gsheetsgapi...") 
+        service_sheet = build('sheets', 'v4', credentials=credentials) 
+        gc = gspread.authorize(credentials) 
+        print("Credentials authorized gsheetsgapi.") 
+    except Exception as e: 
+        print(f"Error authorizing credentials: {e}")
+
+    for i, chunk in enumerate(chunks): 
+        credentials = credentials_list[i % len(credentials_list)]
+        try:
+            print("Appending data to spreadsheet...") 
+            file = file_objects[i % len(file_objects)]  # выбираем соответствующий файл для чанка
+            spreadsheet_id = spreadsheet.id  # get the spreadsheet ID from the spreadsheet object 
+            worksheet = spreadsheet.worksheet("transit") 
+            worksheet_id = worksheet.id  # get the worksheet ID from the worksheet object 
+            append_datagapi(chunk, service_sheet, spreadsheet_id, worksheet_id) 
+            print("Data appended.") 
+        except Exception as e: 
+            logging.error(f"Error appending data to spreadsheet: {e}")  
+            continue
+    print("Done uploading files.") 
+    return spreadsheet_id
+
+def append_datagapi(df, service_sheet, spreadsheet_id, worksheet_id, chunk_size=50000): 
     # Разделите df на подчанки размером chunk_size строк  
     chunks = [df[i:i + chunk_size] for i in range(0, df.shape[0], chunk_size)]  
 
@@ -277,7 +326,7 @@ def append_datagapi(df, service, spreadsheet_id, worksheet_id, chunk_size=50000)
             start_row = i * chunk_size + 1 
             chunk_str = chunk.astype(str) 
             chunk_list = chunk_str.values.tolist() 
-            request = service.spreadsheets().values().append( 
+            request = service_sheet.spreadsheets().values().append( 
                 spreadsheetId=spreadsheet_id, 
                 range=f'transit!A{start_row}',  # Here we define the starting cell for each chunk
                 valueInputOption='USER_ENTERED', 
@@ -289,51 +338,4 @@ def append_datagapi(df, service, spreadsheet_id, worksheet_id, chunk_size=50000)
         except Exception as e: 
             logging.error(f"Error appending chunk {i+1} to the worksheet: {e}") 
             continue 
-        time.sleep(10) 
-
-def upload_to_gsheetsgapi(credentials, spreadsheet, chunks): 
-    logging.info("Authorizing credentials...") 
-    service = build('sheets', 'v4', credentials=credentials) 
-    gc = gspread.authorize(credentials) 
-    logging.info("Credentials authorized.") 
-
-    spreadsheet_id = spreadsheet.id  # get the spreadsheet ID from the spreadsheet object 
-    worksheet = spreadsheet.worksheet("transit") 
-    worksheet_id = worksheet.id  # get the worksheet ID from the worksheet object 
-
-    logging.info("Appending data to spreadsheet...") 
-    try: 
-        for chunk in chunks: 
-            append_datagapi(chunk, service, spreadsheet_id, worksheet_id) 
-    except Exception as e: 
-        logging.error(f"Error appending data to spreadsheet: {e}") 
-        return 
-    logging.info("Data appended.") 
-
-    # Rename the sheet after all chunks have been processed 
-    logging.info("Renaming sheet to 'ready'...") 
-    try: 
-        request = service.spreadsheets().batchUpdate( 
-            spreadsheetId=spreadsheet_id, 
-            body={ 
-                "requests": [ 
-                    { 
-                        "updateSheetProperties": { 
-                            "properties": { 
-                                "sheetId": worksheet_id,  # Change to the ID of your sheet 
-                                "title": "ready" 
-                            }, 
-                            "fields": "title" 
-                        } 
-                    } 
-                ] 
-            } 
-        ) 
-        response = request.execute() 
-    except Exception as e: 
-        logging.error(f"Error renaming sheet: {e}") 
-
-    logging.info("Done uploading files.") 
-    return None
-
-
+        time.sleep(1) 
